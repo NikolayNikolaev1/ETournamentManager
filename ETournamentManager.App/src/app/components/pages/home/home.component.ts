18709e1cb4b23f41d253e56b8490a5d01fe6a0ad
Nikolay Nikolaev <nikolay@we-lovedigi.com>
2024-11-24 16:40:53 +0200

    Redesign

--- ETournamentManager.App/src/app/components/pages/home/home.component.ts
+++ ETournamentManager.App/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { TournamentRounds } from 'app/models/tournament.model';
 import { ApiService } from 'app/services/api.service';
@@ -9,16 +10,42 @@ import { SERVER_ROUTES } from 'app/utils/constants';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   startedTournaments: TournamentRounds[] = [];
   currentTournamnetIndex: number = 0;
+  opacity: number = 0;
+  opacityUp: boolean = true;
+  liveInterval: any;
 
-  constructor(private apiServie: ApiService) {}
+  constructor(
+    private router: Router,
+    private apiServie: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.apiServie
       .request<TournamentRounds[]>({ url: SERVER_ROUTES.TOURNAMENT.GET_ALL_ROUNDS, method: 'get' })
       .subscribe((response) => (this.startedTournaments = response));
+
+    this.liveInterval = setInterval(() => {
+      if (this.opacityUp) {
+        this.opacity++;
+      } else {
+        this.opacity--;
+      }
+
+      if (this.opacity === 9) {
+        this.opacityUp = false;
+      }
+
+      if (this.opacity === 0) {
+        this.opacityUp = true;
+      }
+    }, 100);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.liveInterval);
   }
 
   changeTournament(forward: boolean) {
@@ -31,5 +58,13 @@ export class HomeComponent implements OnInit {
     }
 
     forward ? this.currentTournamnetIndex++ : this.currentTournamnetIndex--;
+  }
+
+  onTeamClick(id: string) {
+    this.router.navigate([`team/${id}`]);
+  }
+
+  onTournamentClick() {
+    this.router.navigate([`tournament/${this.startedTournaments[this.currentTournamnetIndex].id}`]);
   }
 }
